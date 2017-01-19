d5804efc5eb7ec13127d333f603e27b2bc42aebc
KavinduChamara <kavindu.ce@gmail.com>
2017-01-19 15:58:30 +0530

    updated

--- application/views/signin2.php
+++ application/views/signin2.php
@@ -6,22 +6,41 @@
 <head>
   	<title>SIGNIN</title>
   	<meta charset="utf-8">
+  	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  	<link rel="stylesheet" href="<?php echo base_url('public/bootstrap/css/bootstrap.css'); ?>">
+  	<link rel="stylesheet" href="<?php echo base_url('public/css/style.css'); ?>">
     <meta name="description" content="Your description">
     <meta name="keywords" content="Your keywords">
     <meta name="author" content="Your name">
-    <link rel="icon" href="<?php echo base_url('public/images/favicon.ico'); ?>" type="image/x-icon">    
+    <link rel="icon" href="<?php echo base_url('public/images/favicon.ico'); ?>" type="image/x-icon">
 	<link rel="shortcut icon" href="<?php echo base_url('public/images/favicon.ico'); ?>" type="image/x-icon" />
     <link rel="stylesheet" href="<?php echo base_url('public/css/style.css'); ?>">
     <script src="<?php echo base_url('public/js/jquery-1.7.1.min.js'); ?>"></script>
     <script src="<?php echo base_url('public/js/script.js'); ?>"></script>
-<link rel="stylesheet" href="<?php echo base_url('public/bootstrap/css/bootstrap.css'); ?>">	
+<link rel="stylesheet" href="<?php echo base_url('public/bootstrap/css/bootstrap.css'); ?>">
+<style>
+
+form{
+    max-width: 500px;
+    margin: 2% auto;
+    padding: 10px 30px;
+    background: #e4f0ff;
+    border-radius: 20px;
+}
+label{
+    display: block;
+    margin-bottom: 8px;
+}
+
+</style>
+
 
 </head>
 <body id="page7">
 <div class="bg">
 
     <header>
-    	<div class="main">        	
+    	<div class="main">
             <nav>
                 <ul class="sf-menu">
                     <li><a href="index-2.html">company</a></li>
@@ -47,17 +66,17 @@
                     <li class="active"><a href="index-5.html">creations</a></li>
                     <li><a href="index-6.html">mail</a></li>
                 </ul>
-            </nav>            
-            <div class="clear"></div>            
+            </nav>
+            <div class="clear"></div>
         </div>
         <div class="header-box2">
         	<div class="header-box3">
-            <div class="main">           
+            <div class="main">
             	<h1><a class="logo" href="index-2.html">Digital print</a></h1>
                 <div class="inner1">
                 	<span>Contact Us</span><i><br> +94 702 470 762</i>
                     <strong>our expert Staff Is Standing By to answer your questions</strong>
-                </div>                
+                </div>
             </div>
             	<div class="clear"></div>
             </div>
@@ -65,21 +84,30 @@
         </div>
     </header>
 
+<section class="container">
+
+    <div class="login">
 
     <section id="contentnew">
         <div class="main">
             <div class="container_24">
-                <div class="wrapper">  
+                <div class="wrapper">
 
-    <div style=" margin-left:20%; margin-top: 6%; border: solid; width:30%; background-color: #ccdef9; " >
-<form action="<?php echo base_url('index.php/Login/signin'); ?>" method="post">
-
+    <!--<div style=" margin-left:10%; margin-top: 6%; border: solid; width:60%; background-color: #ccdef9; " >-->
+<form class="form-horizontal" action="<?php echo base_url('index.php/Login/signin'); ?>" method="post">
+<h3><align ="centre">SIGIN</h3>
+<div class="form-group">
 	<label >User ID :</label>
 		<input type="text" name="inputuid">
+		</div>
+
+		<div class="form-group">
 	<label >Password :</label>
 		<input type="password" name="inputpwrd">
+		</div>
+		<div class="form-group">
 	<button type="submit">Login</button>
-	
+	</div>
 
 </form>
 <div>
@@ -87,6 +115,8 @@
 	<a href="signup"><button>Signup</button></a>
 </div>
 </div>
+</div>
+</section>
 </body>
 
 
